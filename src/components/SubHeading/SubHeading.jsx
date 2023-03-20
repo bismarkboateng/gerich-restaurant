d6a93b93c50a7471fbb228b247e33b61dfc55d8a
bismarkboateng <bismarkb609@gmail.com>
2023-03-20 06:36:41 +0000

    heaader complete

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { images } from '../../constants';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+const SubHeading = ({ text }) => (
+  <div className="app__subheading">
+    <p className="p__cormorant"> { text } </p>
+    <img src={images.spoon} alt="spoon image" />
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header">
+    <div className="app__header-content">
+      <SubHeading text="Chase The New Flavour" />
+      <h1 className="app__header-content_h1">
+        THE KEY TO A FINE <br />
+        DINING
+      </h1>
+      <p className="app__header-content_p">
+      Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit.<br />
+      Maxime Mollitia, Molestiae Quas Vel Sint Commodi <br />
+      Repudiandae Consequuntur Voluptatum Laborum Numquam<br />
+      Blanditiis Harum Quisquam Eius Sed Odit Fugiat Iusto Fuga
+      Praesentium
+      </p>
+      <button type="submit">Explore Menu</button>
+    </div>
+
+    <div className="app__header-img">
+      <img src={images.welcome} alt="welcome image" />
+    </div>
+
   </div>
 );
 
