19bd017de543773ddb83cf2d45384ca3537a74ec
bismarkboateng <bismarkb609@gmail.com>
2023-03-24 12:08:32 +0000

    added gallery, image slider section

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import { SubHeading } from '../../components';
 import './Gallery.css';
+import { images } from '../../constants';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+
+const GalleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
+
+
+
+const Gallery = () => {
+  const scrollRef = useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === "left") {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollRight += 300;
+    }
+  }
+
+  return (
+    <div className="app__gallery">
+      <div className="app__gallery-content">
+          <SubHeading text="Instagram" />
+          <h1 className="headtext__cormorant">Photto Gallery</h1>
+          <p>
+          Quo Neque Error Repudiandae Fuga? <br />
+          Ipsa Laudantium Molestias Eos Sapiente <br />
+          Officiis Modi At Sunt Excepturi Expedita Sint? <br />
+          Sed Quibusd <br />
+          </p>
+          <button className="custom__button">View More</button>
+      </div>
+
+      <div className="app__gallery-ImageSlider">
+        {
+          GalleryImages.map((img) => (
+            <>
+              <img src={img} className="app__gallery-ImageSlider-img" alt="images" />
+              <BsInstagram />
+            </>
+
+          ))
+        }
+      </div>
+      <BsArrowLeftShort 
+      fontSize={35} 
+      className="app__gallery-leftArrow" 
+      style={{color: "white"}}
+      onClick={() => scroll("left")}
+      />
+      
+      <BsArrowRightShort 
+      fontSize={35} 
+      className="app__gallery-RightArrow" 
+      style={{color: "black"}}
+      onClick={() => scroll("right")}
+      />
+   </div>
+  );
+
+}
 
 export default Gallery;
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
-
+import { images, data } from '../../constants';
+import { SubHeading } from '../../components';
 import './Laurels.css';
 
+
+const AwardCard = ({info : {imgUrl, title, subtitle}}) => (
+    <div className="app__laurels-info_content">
+      <div className="app__laurels-imgs">
+        <img src={imgUrl} alt="awards" />
+      </div>
+
+      <div className="app__laurels-content">
+        <p className="p__cormorant">{title}</p>
+        <p>{subtitle}</p>
+      </div>
+
+     </div>
+)
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__laurels app__bg">
+
+    <div className="app__laurels-info">
+      <SubHeading text="Awards & Recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+      {
+        data.awards.map((info) => <AwardCard info={info} key={info.title} />) 
+      }
+    </div>
+
+    <div className="app__laurels-img">
+      <img src={images.laurels} alt="laurels image" />
+    </div>
+
   </div>
 );
 
